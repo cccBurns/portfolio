a3e88ba7d626c0a0909f99b7e2136841dbcbe03d
christian burns <christianburns03@gmail.com>
2024-09-23 10:51:07 -0300

    lunes 23 a

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ import datetime
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=250)
+    descripcion = models.TextField(max_length=999)
     imagen = models.ImageField(upload_to='blog/imagen')
     date = models.DateField(datetime.date.today)
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='imagenes')
+    imagen = models.ImageField(upload_to='blog/imagen')
--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,5 @@ def render_posts(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {"post": post} )
+    images = post.imagenes.all()  # Accede a las imágenes relacionadas
+    return render(request, 'post_detail.html', {"post": post, "images": images})
